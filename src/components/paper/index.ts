81f1f64dc78fa99cc6e6ac58d1ec58201b1e5f6f
Fypoolday <Fypoolday@users.noreply.github.com>
2022-07-12 23:03:58 -0300

    componente hand-papper creado

--- src/components/paper/index.ts
+++ src/components/paper/index.ts
@@ -1,3 +1,5 @@
+const papel = require("url:../../../src/assets/papper-hand.png");
+
 customElements.define(
   "hand-paper",
   class Button extends HTMLElement {
@@ -7,6 +9,19 @@ customElements.define(
       this.shadow = this.attachShadow({ mode: "open" });
       this.render();
     }
-    render() {}
+    render() {
+      const style = document.createElement("style");
+      this.shadow.innerHTML = `
+        <img class="hand-paper" src="${papel}">
+        `;
+
+      style.innerHTML = `
+            .hand-paper {
+                height: 100%;
+                width: 100%
+            }
+            `;
+      this.shadow.appendChild(style);
+    }
   }
 );
